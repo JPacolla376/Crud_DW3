efa97ae432681b236a99766f70a39fb635278cc0
João Augusto S Pacolla <127799303+JPacolla376@users.noreply.github.com>
2024-12-05 18:21:35 -0300

    update

--- suporte_chamados/suporte_chamados/urls.py
+++ suporte_chamados/suporte_chamados/urls.py
@@ -16,24 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.auth import views as auth_views
-from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),  # Página inicial
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('cadastro/', views.cadastro, name='cadastro'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('lista_chamados/', views.lista_chamados, name='lista_chamados'),  # Página para listar chamados
-    path('criar_chamado/', views.criar_chamado, name='criar_chamado'),  # Criar chamado
-    path('detalhes_chamado/<int:id>/', views.detalhes_chamado, name='detalhes_chamado'),  # Detalhes do chamado
-    path('editar_chamado/<int:id>/', views.editar_chamado, name='editar_chamado'),  # Editar chamado
-    path('deletar_chamado/<int:id>/', views.deletar_chamado, name='deletar_chamado'),  # Deletar chamado
-     path('chamados/', include('chamados.urls'))
+    path('', include('chamados.urls'))
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 
